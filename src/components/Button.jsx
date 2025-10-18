846475fece88eca6185796677fc1b486d3392c26
Amir Hossein Kargar <amirkaargar@hotmail.com>
2025-10-19 02:47:34 +0330

    feat(button): Add secondary variant and improve button styling
    - Introduce new `variant` prop with `primary` and `secondary` options
    - Refactor button styles to support different visual variants
    - Update disabled state handling for both primary and secondary variants
    - Improve button component flexibility and styling consistency
    - Enhance button component with more dynamic styling options

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
-export default function Button({ size = "small", children, disabled = false }) {
-    const sizes = {
-        small: `py-1.5 px-4 text-xs`,
-        medium: `py-2.5 px-4 text-sm`,
-        large: `py-3.5 px-6 text-base`,
-    }
+export default function Button({ size = "small", variant = "secondary", children, disabled = false }) {
+  const sizes = {
+    small: `py-1.5 px-4 text-xs`,
+    medium: `py-2.5 px-4 text-sm`,
+    large: `py-3.5 px-6 text-base`,
+  }
 
-    return (
-        <button
-            disabled={disabled}
-            className={`${sizes[size]} text-white font-semibold rounded-sm bg-primary-500 active:bg-primary-700 ${disabled ? 'opacity-40 cursor-not-allowed' : 'cursor-pointer'} flex justify-center items-center border border-transparent focus:border-button-border-active transition-colors `}
-        >
-            {children}
-        </button>
-    )
+  const variants = {
+    primary: `text-white bg-primary-500 active:bg-primary-700 border-transparent border focus:border-button-border-active ${disabled ? 'opacity-40 cursor-not-allowed' : 'cursor-pointer'}`,
+    secondary: `bg-white border text-secondary-400 hover:border-secondary-400 hover:text-secondary-500 active:bg-primary-100 active:border-secondary-300 ${disabled ? 'border-primary-100 cursor-not-allowed' : 'border-secondary-300 cursor-pointer'}`
+  }
+
+  return (
+    <button
+      disabled={disabled}
+      className={`${sizes[size]} ${variants[variant]} font-semibold rounded-sm flex justify-center items-center transition-colors`}
+    >
+      {children}
+    </button>
+  )
 }
